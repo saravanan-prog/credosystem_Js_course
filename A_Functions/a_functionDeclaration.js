3c69654a5eb50b94772e8bb1b2f82dd7bda00011
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-04-11 09:41:24 +0530

    initiate commit

---
+++ A_Functions/a_functionDeclaration.js
@@ -0,0 +1,40 @@
+/* Example 1  */
+
+function greet() {
+    
+    return "Welcome to Javascript world";
+}
+
+let greetResult = greet()
+
+
+/* Example 2 */
+
+function additionCalculation() {
+    let a = 100;
+    let b = 500
+    let c = a + b
+    return c;
+}
+
+let additionResult = additionCalculation()
+
+
+/* Example 3 */
+
+function sumofAverageCalculation() {
+    let a = 100;
+    let b = 500
+    let c = a + b
+    let avg = c / 2;
+    return avg ;
+}
+
+let averageResult = sumofAverageCalculation()
+
+
+
+
+
+
+console.log("greetResult=====>",greetResult)
--- A_conditonStatement/a_simpleIFStatment.js
+++
@@ -1,7 +0,0 @@
-let age = 16;
-
-if (age >= 18) {
-    console.log("Adult");
-} else {
-    console.log("Minor");
-}
