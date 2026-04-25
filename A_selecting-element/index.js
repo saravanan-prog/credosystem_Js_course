98314a30cffa06c54d64ba738afc104f6021663b
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-04-25 22:52:18 +0530

    selector

--- A_selecting-element/index.js
+++ A_selecting-element/index.js
@@ -1,11 +1,1 @@
 
-const firstElement = document.getElementById("firstId")
-const content = document.getElementsByClassName('content')[1]
-const pragraphTag = document.getElementsByTagName("p")
-
-const querySelector = document.querySelector(".content")  
-const querySelectorAll = document.querySelectorAll("#firstId")
-
-
-
-console.log("querySelectorAll ===>",querySelectorAll)
