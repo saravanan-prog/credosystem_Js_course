5b37b156d5ac2586b66d5973afdf90492d217d03
sarvanan-prog <saravanan.mca1992@gmail.com>
2026-05-02 12:30:08 +0530

    doc submission

--- D_form-handling/index.js
+++ D_form-handling/index.js
@@ -1,48 +1,41 @@
 
-
-
-
-
-function formValidate(){
-
+function formValidate(event){
     event.preventDefault();
-    const userName = document.getElementById("username").value;
-    const errorMessage = document.getElementsByClassName('username_erromsg')[0]
 
-    console.log("userName",userName)
+    const userName = document.getElementById("username").value;
+    const usernameError = document.getElementById('username_error')
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+
+    //Password selector
+
+
+
     if(userName.trim() === null || userName.trim() === ''){
         
-        errorMessage.innerHTML = `
-            <span style=color:red> Please enter your mobile number  </span>
+        usernameError.innerHTML = `
+            <span style=color:red> Please enter your  username  </span>
         `
     }
 
     else if(userName.length < 3){
 
-        console.log("Entered secnd if")
-        
-        errorMessage.innerHTML = `
+        usernameError.innerHTML = `
             <span style=color:red> Username must be minimum 3 letter required  </span>
         `
     }
 
-       else if(userName.length > 5){
-
-       
-        
-        errorMessage.innerHTML = `
-            <span style=color:red> Username not exclude 6 char  </span>
+    else if(!pattern.test(userName)){
+        usernameError.innerHTML = `
+            <span style=color:red> username is must be email format  </span>
         `
     }
-
-
-
 
 
 
     else{
         
-        errorMessage.innerHTML =``
+        usernameError.innerHTML =``
 
         let formdata = {
             username : userName
