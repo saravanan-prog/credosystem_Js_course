3bd6f5a00ad7d0ec59b6f2ca6742fe254c2587f2
saravanan-prog <saravanan.mca1992@gmail.com>
2026-07-27 09:42:51 +0530

    workouts

--- firstProgram.js
+++
@@ -1,8 +0,0 @@
-let fruit = ["apple","orange"]                                       // 2000
-
-let newFruit =  fruit              // deep copy                      // 2000
-
-newFruit[0] = null;
-
-console.log(fruit)
-console.log(newFruit)
