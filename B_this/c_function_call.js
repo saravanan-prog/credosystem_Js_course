475435f7bcf46a0b3933cb11ac33ae0da054f91c
saravanan-prog <saravanan.mca1992@gmail.com>
2026-07-29 09:22:47 +0530

    fn concept

--- B_this/c_function_call.js
+++ B_this/c_function_call.js
@@ -1,13 +1,13 @@
-
-let product = {
-    productname: "apple",
-    productPrice: 20
-}
-
 function getProductDetails(manifactireUnit){
     console.log("Product Name====>",this.productname)
     console.log("Product Price====>",this.productPrice)
     console.log("Mainfacture unit====>",manifactireUnit)
 }
 
+
+
+let product = {
+    productname: "apple",
+    productPrice: 20
+}
 getProductDetails.call(product,"himachala")
