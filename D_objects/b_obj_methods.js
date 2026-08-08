df917626cc97c4a21777445b1b28b9c5dd58fbef
saravanan-prog <saravanan.mca1992@gmail.com>
2026-08-08 23:38:31 +0530

    obj method

--- D_objects/b_obj_methods.js
+++ D_objects/b_obj_methods.js
@@ -11,10 +11,15 @@
     };
     
     
-    let keys = Object.keys(user)
-    let values = Object.values(user)
-    let entires = Object.entries(user);
+    let keys    =  Object.keys(user)           // Retrive the keys
+    let values  =  Object.values(user)         // Retrive the values
+    let entires =  Object.entries(user);       //  Retrive the entries sub-array
+
+    let stringData   = JSON.stringify(user)    // Object to convert string
+    let strToObject  = JSON.parse(stringData)  // string to convert object
 
     console.log("keys===>",keys)
     console.log("values===>",values)
-     console.log("entires===>",entires)
+    console.log("entires===>",entires)
+    console.log("stringData===>",typeof stringData)
+    console.log("strToObject===>",typeof strToObject)
