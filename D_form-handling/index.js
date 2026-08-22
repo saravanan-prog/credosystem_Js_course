a0f2bdea8794216be1877c2cfd5599faedb0fb95
saravanan-prog <saravanan.mca1992@gmail.com>
2026-08-22 23:40:38 +0530

    upgrade unit 7

--- D_form-handling/index.js
+++ D_form-handling/index.js
@@ -1,45 +1,5 @@
 
-function formValidate(event){
-
-    event.preventDefault();
-
-    const userName      = document.getElementById("username").value;
-    const usernameError = document.getElementById('username_error')
-    const pattern       = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-
-
-
-
-    if(!userName){
-        usernameError.innerHTML = `
-            <span style=color:red> Please enter your  username  </span>
-        `
-    }
-
-    else if(userName.length < 3){
-
-        usernameError.innerHTML = `
-            <span style=color:red> Username must be minimum 3 letter required  </span>
-        `
-    }
-    else if(!pattern.test(userName)){
-        usernameError.innerHTML = `
-            <span style=color:red> username is must be email format  </span>
-        `
-    }
-
-    else{
-        
-        usernameError.innerHTML =``
-
-        let formdata = {
-            username : userName
-        }
-
-        console.log("formdata",formdata)
-    }
-    
-
-   
+function formValidate(e){
+  
 }
 
